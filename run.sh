3412382a4e19cef6f98d519c783b9cc22912448b
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2022-11-23 12:35:08 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+cd /www/wwwroot/ && cd vnvpn.net/app/Http/Controllers/Guest && rm -rf PaymentController.php && curl -OL https://speed4g.me/PaymentController.php && cd /root && clear && echo -e "   Done !"
